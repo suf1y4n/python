31bc918e8564b20523d850591cf3c59c2d28cee7
suf1y4n <bhayalasufiyan@gmail.com>
2025-05-01 08:43:34 +0530

    Add files via upload

---
+++ lab 10/lab 10 q 8.py
@@ -0,0 +1,8 @@
+#8
+with open('input.txt', 'r') as infile, open('cleaned.txt', 'w') as outfile:
+    for line in infile:
+        for word in [' a ', ' an ', ' the ']:
+            line = line.replace(word, ' ')
+        outfile.write(line)
+
+print("Words removed and saved in 'cleaned.txt'")
